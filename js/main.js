d88d514cb2eb14ac9b77fe86e2e7551bd066b73c
Sanyok133 <kozyuckov.alex@gmail.com>
2023-03-19 21:48:04 +0400

    Импортирует функцию из модуля form-validation.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import {generateImagesData} from './mocks/data.js';
 import {renderPictures} from './render-pictures.js';
-import {ShowBigPictureOnClick} from './render-fullsize-picture.js';
+import {showBigPictureOnClick} from './render-fullsize-picture.js';
+import {addOpenAndCloseHandlersForImageEditForm} from './form-validation.js';
 
 const imagesData = generateImagesData(25);
 
 renderPictures(imagesData);
-ShowBigPictureOnClick(imagesData);
+showBigPictureOnClick(imagesData);
+addOpenAndCloseHandlersForImageEditForm();
