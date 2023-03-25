4ae867d5bae75adc4a7939bbb1bbd3a97c77c1df
Sanyok133 <kozyuckov.alex@gmail.com>
2023-03-25 11:31:19 +0400

    refactor: Убирает лишние условия в функциях для масштабирования изображений

--- js/scale-image.js
+++ js/scale-image.js
@@ -7,35 +7,34 @@ const increaseButton = document.querySelector('.scale__control--bigger');
 const scaleControl = document.querySelector('.scale__control--value');
 const imagePreview = document.querySelector('.img-upload__preview');
 
-
-const addImageScaleHandlers = () => {
-  reduceButton.addEventListener('click', () => {
-    let scaleValue = parseInt(scaleControl.value, 10);
-    if (scaleValue >= MIN_VALUE) {
-      scaleValue -= STEP;
-      if (scaleValue < MIN_VALUE) {
-        scaleValue = MIN_VALUE;
-      }
-      scaleControl.value = `${scaleValue}%`;
-      imagePreview.style.transform = `scale(0.${scaleValue})`;
-    }
-  });
-
-  increaseButton.addEventListener('click', () => {
-    let scaleValue = parseInt(scaleControl.value, 10);
-    if (scaleValue <= MAX_VALUE) {
-      scaleValue += STEP;
-      if (scaleValue > MAX_VALUE) {
-        scaleValue = MAX_VALUE;
-      }
-      scaleControl.value = `${scaleValue}%`;
-      if (scaleValue === MAX_VALUE) {
-        imagePreview.style.transform = 'scale(1)';
-      } else {
-        imagePreview.style.transform = `scale(0.${scaleValue})`;
-      }
-    }
-  });
+const onReduceScaleButtonClick = () => {
+  let scaleValue = parseInt(scaleControl.value, 10);
+  scaleValue -= STEP;
+  if (scaleValue < MIN_VALUE) {
+    scaleValue = MIN_VALUE;
+  }
+  scaleControl.value = `${scaleValue}%`;
+  imagePreview.style.transform = `scale(${scaleValue / 100})`;
 };
 
-export {addImageScaleHandlers};
+const onIncreaseButtonClick = () => {
+  let scaleValue = parseInt(scaleControl.value, 10);
+  scaleValue += STEP;
+  if (scaleValue > MAX_VALUE) {
+    scaleValue = MAX_VALUE;
+  }
+  scaleControl.value = `${scaleValue}%`;
+  imagePreview.style.transform = `scale(${scaleValue / 100})`;
+};
+
+const addImageScaleHandlers = () => {
+  reduceButton.addEventListener('click', onReduceScaleButtonClick);
+  increaseButton.addEventListener('click', onIncreaseButtonClick);
+};
+
+const removeImageScaleHandlers = () => {
+  reduceButton.removeEventListener('click', onReduceScaleButtonClick);
+  increaseButton.removeEventListener('click', onIncreaseButtonClick);
+};
+
+export {addImageScaleHandlers, removeImageScaleHandlers};
